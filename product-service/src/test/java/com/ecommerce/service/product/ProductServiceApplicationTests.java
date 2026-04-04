b61addc1a55432225b7920e63a198773892b1e2a
game <hhhau1910@gmail.com>
2026-04-04 10:45:45 +0700

    feat: initialize product service with Maven setup and basic application structure

---
+++ product-service/src/test/java/com/ecommerce/service/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.service.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
